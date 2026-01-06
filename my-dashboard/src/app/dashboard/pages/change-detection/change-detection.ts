7b981cf7c88b545b22942412d434698747d25ecb
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2026-01-06 15:42:45 -0400

    Enhance control flow component with additional framework lists and update title styling

--- my-dashboard/src/app/dashboard/pages/change-detection/change-detection.ts
+++ my-dashboard/src/app/dashboard/pages/change-detection/change-detection.ts
@@ -1,10 +1,43 @@
-import { Component } from '@angular/core';
+import {ChangeDetectionStrategy, Component, signal} from '@angular/core';
+import {Title} from '../../../share/title/title';
+import {CommonModule, JsonPipe} from '@angular/common';
 
 @Component({
-  selector: 'app-change-detection',
-  imports: [],
-  templateUrl: './change-detection.html',
+  standalone: true,
+  imports: [
+    Title,
+    JsonPipe,
+    CommonModule
+  ],
+  changeDetection:ChangeDetectionStrategy.OnPush,
+  template: ` <app-title title="Change Dections" />
+     <pre> {{frameWorkSignal() | json}} </pre>
+     <pre> {{frameWorkProperty | json}} </pre>
+
+
+    `
 })
 export default class ChangeDetection {
+  public frameWorkSignal = signal(
+    {
+      name: 'Angular',
+      relaseDate:2016
+    }
+  );
+  public frameWorkProperty={
+    name:'Angular de property',
+    relaseDate:2016
+  }
 
+  constructor() {
+
+    setTimeout(() => {
+      //this.frameWorkProperty.name='React';
+      this.frameWorkSignal.update(value => ({
+        ...value,
+        name: 'React'
+      }))
+      console.log('hecho');
+    },2000);
+  }
 }
