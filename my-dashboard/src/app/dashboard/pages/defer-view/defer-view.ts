e9b7f43263d4d7dab17ead8160b557de9f381565
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2026-01-05 20:54:16 -0400

    Add control-flow, defer-options, and defer-view components with templates and tests

---
+++ my-dashboard/src/app/dashboard/pages/defer-view/defer-view.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-defer-view',
+  imports: [],
+  templateUrl: './defer-view.html',
+  styleUrl: './defer-view.css',
+})
+export class DeferView {
+
+}
