5f6898e5b1d645fc85dfa5b33205ff00e93952b2
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2026-01-05 21:12:06 -0400

    Add user, users, and view-transitions components with templates and tests

--- my-dashboard/src/app/share/heavy-loaders-slow/users-loader.component.ts
+++ my-dashboard/src/app/share/heavy-loaders-slow/users-loader.component.ts
@@ -3,8 +3,9 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-user-loader',
   standalone: true,
+  imports: [],
   templateUrl: `<h1> Nombre del Usuario </h1> `,
 })
-export class NameComponent {
+export class UserLoadersFast {
 
 }
