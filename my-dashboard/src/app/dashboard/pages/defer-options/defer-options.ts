7899017e188bdae77c5db27ff1a333af5e0d3937
Fernando Catacora <fernando.catacora@ucb.edu.bo>
2026-01-08 22:01:32 -0400

    Enhance defer-options component with title and section layout

--- my-dashboard/src/app/dashboard/pages/defer-options/defer-options.ts
+++ my-dashboard/src/app/dashboard/pages/defer-options/defer-options.ts
@@ -1,8 +1,13 @@
 import { Component } from '@angular/core';
+import {CommonModule} from '@angular/common';
+import {HeavyLoadersFast} from '../../../share/heavy-loaders-slow/heavy-loaders-fast';
+import {Title} from '../../../share/title/title';
+
 
 @Component({
   selector: 'app-defer-options',
-  imports: [],
+  imports: [CommonModule, HeavyLoadersFast, Title],
+  standalone: true,
   templateUrl: './defer-options.html',
 
 })
--- my-dashboard/src/app/share/heavy-loaders-slow/heavy-loaders-fast.ts
+++ my-dashboard/src/app/share/heavy-loaders-slow/heavy-loaders-fast.ts
@@ -1,11 +1,17 @@
-import { Component } from '@angular/core';
+import {Component, Input} from '@angular/core';
+import {NgClass} from '@angular/common';
 
 @Component({
   selector: 'app-heavy-loaders-fast',
   standalone: true,
-  imports: [],
-  template: `<h1>Heavy Loader Fast</h1>`,
+  imports: [
+    NgClass
+  ],
+  template: `
+    <section [ngClass]="['w-full', cssClass]" >
+      <ng-content/>
+    </section>`,
 })
 export class HeavyLoadersFast {
-
+  @Input({ required : true }) cssClass!: string ;
 }
